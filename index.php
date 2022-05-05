c7dd6c1017b5026a818d5ebba1d9b1baf5175679
Patrick Luong <pluonggr@srv603.webhostingforstudents.com>
2022-05-05 12:25:09 -0400

    index composer and .gitignore

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+//Turn on error reporting
+ini_set('display_error', 1);
+error_reporting(E_ALL);
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+//:: is used to call a method within the static Base class within fat-free
+$f3 = Base::instance();
+
+//Define a default route
+//
+$f3->route('GET /', function($f3) {
+
+    $view = new Template();
+    echo $view->render('views/info.html');
+});
+
+//Run fat free
+// -> is invoking the run() method in the fat-free
+$f3->run();
