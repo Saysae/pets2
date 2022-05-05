158c3263d0680fdb4d65e9695056f63dbc339123
Patrick Luong <pluonggr@srv603.webhostingforstudents.com>
2022-05-05 13:42:49 -0400

    final

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ error_reporting(E_ALL);
 //Require the autoload file
 require_once('vendor/autoload.php');
 
+//start a session
+session_start();
+
 //Create an instance of the Base class
 //:: is used to call a method within the static Base class within fat-free
 $f3 = Base::instance();
@@ -26,12 +29,20 @@ $f3->route('GET /order', function() {
 });
 
 //Order2 route
-$f3->route('GET|POST /order2', function() {
-
+$f3->route('POST /order2', function() {
+    var_dump($_POST);
     $_SESSION['pet'] = $_POST['pet'];
     $_SESSION['color'] = $_POST['color'];
     $view = new Template();
     echo $view->render('views/pet-order2.html');
+});
+
+//Summary
+$f3->route('POST /summary', function() {
+    var_dump($_POST);
+    $_SESSION['name'] = $_POST['name'];
+    $view = new Template();
+    echo $view->render('views/summary.html');
 });
 
 //Run fat free
